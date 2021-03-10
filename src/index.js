429b2ba99ef4ca9612e374058c270e6fb01abc48
elirandagan <elirandagan123@gmail.com>
2021-03-10 21:49:45 +0200

    Adding login page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app_port = process.env.PORT || 3000
 const app = express()
+const router = express.Router()
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+
+router.get('/',function(req,res){
+  res.status(200).render('login')
 })
 
-app.listen(app_port)
-console.log(`app is running. port: ${app_port}`)
-console.log(`http://127.0.0.1: ${app_port}`)
+// router.get('/login',function(req,res){
+//   res.status(200).render('login')
+// })
+
+
+// add the router
+app.use('/', router)
+module.exports = app.listen(app_port)
+
+console.log(`app is running. port:${app_port}`)
+console.log(`http://127.0.0.1:${app_port}`)
