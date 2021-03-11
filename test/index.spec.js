39b00881860de281d60a6dc2220827ec843f1312
elirandagan <elirandagan123@gmail.com>
2021-03-11 18:43:13 +0200

    add test

---
+++ test/index.spec.js
@@ -0,0 +1,19 @@
+var chai = require("chai");
+var chaiHttp = require("chai-http");
+var server = require("../src/index.js");
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+
+describe("Connect to server", function() {
+
+    it("Login Page(first page) /get", function(done) {
+      chai.request(server)
+      .get("/")
+      .end(function(err, res){
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
