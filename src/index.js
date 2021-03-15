3a9c958ce20b9e43e46dbaf6975e31afc209d469
Dagan Eliran <elan1305@elta.co.il>
2021-03-15 10:21:29 +0200

    Adding Dashbaord template for the next sprint

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
-const express = require('express')
-const app_port = process.env.PORT || 3000
-const app = express()
-const router = express.Router()
+const express = require("express");
+const app_port = process.env.PORT || 3000;
+const app = express();
+const router = express.Router();
 
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs')
+app.use(express.static("public"));
 
-app.use(express.static('public'))
+router.get("/", function (req, res) {
+  res.status(200).render("login");
+});
 
+router.get("/typography", function (req, res) {
+  res.status(200).render("typography");
+});
 
-router.get('/',function(req,res){
-  res.status(200).render('login')
-})
+router.get("/notifications", function (req, res) {
+  res.status(200).render("notifications");
+});
 
-router.get('/dashboard',function(req,res){
-  res.status(200).render('dashboard')
-})
+router.get("/template", function (req, res) {
+  res.status(200).render("template");
+});
 
+router.get("/icons", function (req, res) {
+  res.status(200).render("icons");
+});
 
+router.get("/tables", function (req, res) {
+  res.status(200).render("tables");
+});
+
+router.get("/user", function (req, res) {
+  res.status(200).render("user");
+});
+
+router.get("/map", function (req, res) {
+  res.status(200).render("map");
+});
+
+router.get("/dashboard", function (req, res) {
+  res.status(200).render("dashboard");
+});
 
 //add the router
-app.use('/', router)
-module.exports = app.listen(app_port)
+app.use("/", router);
+module.exports = app.listen(app_port);
 
-console.log(`app is running. port: ${app_port}`)
-console.log(`http://127.0.0.1:${app_port}/`)
+console.log(`app is running. port: ${app_port}`);
+console.log(`http://127.0.0.1:${app_port}/`);
