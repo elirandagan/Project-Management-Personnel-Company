a362da02d1d1a30fe5dc7a41a9468d32e45b225e
Guy Dahan <guydahan1996@gmail.com>
2021-03-15 11:33:52 +0200

    UI

--- src/index.js
+++ src/index.js
@@ -49,15 +49,3 @@ module.exports = app.listen(app_port);
 
 console.log(`app is running. port: ${app_port}`);
 console.log(`http://127.0.0.1:${app_port}/`);
-
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = 'mongodb+srv://Admin:<>@cluster0.6vq53.mongodb.net/ProjectManger?retryWrites=true&w=majority';
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// // eslint-disable-next-line no-unused-vars
-// client.connect(err => {
-//   const collection = client.db('test').collection('devices');
-//   console.log(collection)
-//   // perform actions on the collection object
-//   client.close();
-// });
