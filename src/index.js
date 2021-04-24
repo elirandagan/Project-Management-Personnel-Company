90c18ff2b2131d6b816c6354422709fd1b1d96c8
Guy Dahan <guydahan1996@gmail.com>
2021-04-25 01:33:16 +0300

    mongoDb.js - microservices for monogoDb signup + privacyPolicy pages frontend + backend DONE login In progress (not sync to the DB)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ const bodyParser = require("body-parser");
 const app_port = process.env.PORT || 3000;
 const app = express();
 const router = express.Router();
+const mongoDbFunction = require("./mongoDb");
+
+const validateUser = false
 
 const MongoClient = require("mongodb").MongoClient;
 const uri = "mongodb+srv://EliranDagan123:dagan123@cluster0.aszt8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-MongoClient.connect(uri, { useUnifiedTopology: true })
+MongoClient.connect(uri, {useUnifiedTopology: true})
     .then(client => {
         console.log("Connected to Database")
         const db = client.db("GLEM-TECH")
@@ -21,23 +24,51 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
         app.set("view engine", "ejs");
 
         app.use(express.static("public"));
-        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
 
         // HOW TO "GET" FROM COLLECTION
-        router.get("/", function (req, res) {
-            console.log("Login")
-            HR_Users_Collection.find({ firstName: "Lior" }).toArray(function (err, result) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(JSON.stringify(result));
-                }
-            })
-            console.log("succed")
-
+        router.get("/", async function (req, res) {
+            console.log("HOMEPAGE")
             res.status(200).render("login");
         });
+
+        router.get("/login", function (req, res) {
+            console.log("loginGet")
+            console.log("router get/login",req.body.userName, req.body.password, req.body.identity)
+            mongoDbFunction.validateLogin(req.body.userName, req.body.password, req.body.identity).then(r  =>{
+                console.log("router r : ", r)
+            })
+        });
+
+        router.post("/login", (req, res) => {
+            console.log("router set/login",req.body.userName, req.body.password, req.body.identity)
+            mongoDbFunction.validateLogin(req.body.userName, req.body.password, req.body.identity).then(r  =>{
+                console.log("router r : ", r)
+            })
+        })
+
+        router.get("/signup", function (req, res) {
+            res.status(200).render("signup",{exist: 1});
+        });
+
+
+        router.post("/signup", async (req, res) => {
+            //TODO fix the swal and add more condition
+            if (await (mongoDbFunction.inserToDb("Contractor", req.body))) {
+                res.status(200).render("login", {exist: 0});
+                console.log("router build user")
+            } else {
+                res.status(200).render("signup", {exist: 1});
+                console.log("router Failed user")
+            }
+
+        });
+
+        router.get("/privacyPolicy", function (req, res) {
+            res.status(200).render("privacyPolicy");
+        });
+
 
         router.get("/template", function (req, res) {
             res.status(200).render("template");
@@ -58,37 +89,35 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
 
         router.get("/recruit", function (req, res) {
             console.log("recruit")
-            HR_Users_Collection.find({ firstName: "Lior" }).toArray(function (err, result) {
+            HR_Users_Collection.find({firstName: "Lior"}).toArray(function (err, result) {
                 if (err) {
                     console.log(err);
                 } else {
                     console.log(result);
-                    res.status(200).render("recruit", {exist : 2, ID : 0});
+                    res.status(200).render("recruit", {exist: 2, ID: 0});
                 }
             })
         });
 
         router.post("/recruit", (req, res) => {
-            Contractor_Users_Collection.find({ ID: req.body.ID }).toArray(function (err, result) {
+            Contractor_Users_Collection.find({ID: req.body.ID}).toArray(function (err, result) {
                 console.log("ID: " + JSON.stringify(req.body.ID))
                 console.log("result: " + result.length)
                 if (result.length != 0) {
-                    res.status(200).render("recruit", { exist: 1, ID: req.body.ID });
+                    res.status(200).render("recruit", {exist: 1, ID: req.body.ID});
                     console.log(req.body.ID + " 1 Failed")
-                }
-                else {
-                    Contractor_Users_Collection.find({ userName: req.body.userName }).toArray(function (err, result2) {
+                } else {
+                    Contractor_Users_Collection.find({userName: req.body.userName}).toArray(function (err, result2) {
                         console.log("userName: " + req.body.userName)
                         console.log("result: " + result2.length)
                         if (result2.length != 0) {
-                            res.status(200).render("recruit", { exist: 1, ID: req.body.ID });
+                            res.status(200).render("recruit", {exist: 1, ID: req.body.ID});
                             console.log(result2[0] + "2 Failed")
-                        }
-                        else {
+                        } else {
                             console.log(res + " Succeed")
                             Contractor_Users_Collection.insertOne(req.body)
                                 .then(result => {
-                                    res.status(200).render("recruit", { exist: 0, ID: req.body.ID });
+                                    res.status(200).render("recruit", {exist: 0, ID: req.body.ID});
                                 })
                         }
                     })
@@ -117,31 +146,33 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
         });
 
         router.get("/dashboard", function (req, res) {
-            res.status(200).render("dashboard");
+            validateUser ? res.status(200).render("dashboard") : res.status(200).render("login");
         });
 
-        //DB Actions
 
 
-        // HOW TO "POST" TO COLLECTION
-        // router.post('/user', (req, res) => {
-        //   UsersCollection.insertOne(req.body)
-        //     .then(result => {
-        //       // console.log(req.body)
-        //       console.log(result)
-        //       res.redirect('/dashboard')
-        //     })
-        //     .catch(error => console.error(error))
-        // })
+//DB Actions
 
-        // app.get('/', (req, res) => {
-        //   const cursor = db.collection('Users').find().toArray()
-        //   console.log(cursor)
-        //   // console.log('succed')
-        //   //
-        // })
 
-        //add the router
+// HOW TO "POST" TO COLLECTION
+// router.post('/user', (req, res) => {
+//   UsersCollection.insertOne(req.body)
+//     .then(result => {
+//       // console.log(req.body)
+//       console.log(result)
+//       res.redirect('/dashboard')
+//     })
+//     .catch(error => console.error(error))
+// })
+
+// app.get('/', (req, res) => {
+//   const cursor = db.collection('Users').find().toArray()
+//   console.log(cursor)
+//   // console.log('succed')
+//   //
+// })
+
+//add the router
         app.use("/", router);
 
     })
